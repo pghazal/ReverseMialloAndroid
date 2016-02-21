c75f87a8f23363ae956839ab3a65f38c597072e6
Pierre Ghazal <pg.ghazal@gmail.com>
2016-02-21 17:30:38 +0100

    Managing checkable friends Custom ActionBar

---
+++ app/src/main/java/com/pghazal/reversemiallo/activity/SettingsActivity.java
@@ -0,0 +1,54 @@
+package com.pghazal.reversemiallo.activity;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+
+import com.pghazal.reversemiallo.R;
+import com.pghazal.reversemiallo.utility.SessionManager;
+
+public class SettingsActivity extends AppCompatActivity  implements View.OnClickListener{
+
+    private Animation mShakeAnim;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+
+        findViewById(R.id.logoutButton).setOnClickListener(this);
+        findViewById(R.id.accountButton).setOnClickListener(this);
+
+        mShakeAnim = AnimationUtils.loadAnimation(this, R.anim.shake);
+    }
+
+    private void logout() {
+        if (SessionManager.isLoggedIn(this)) {
+            SessionManager.setLoggedIn(this, false);
+
+            Intent intent = new Intent(this, MainActivity.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(intent);
+            finish();
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.logoutButton:
+                logout();
+                break;
+
+            case R.id.accountButton:
+                v.startAnimation(mShakeAnim);
+                break;
+
+            default:
+                break;
+        }
+    }
+}
---
+++ app/src/main/java/com/pghazal/reversemiallo/fragment/GalleryFragment.java
@@ -0,0 +1,49 @@
+package com.pghazal.reversemiallo.fragment;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.pghazal.reversemiallo.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link GalleryFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class GalleryFragment extends Fragment {
+
+    public GalleryFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @return A new instance of fragment GalleryFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static GalleryFragment newInstance() {
+        GalleryFragment fragment = new GalleryFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_gallery, container, false);
+    }
+
+}
